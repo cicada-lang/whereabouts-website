8564ea735b45c3c8b7259f9109b806010c20c2d0
Xie Yuheng <xyheme@gmail.com>
2022-10-03 17:03:16 +0800

    up

--- src/pages/playground/PlaygroundOutput.vue
+++ src/pages/playground/PlaygroundOutput.vue
@@ -21,7 +21,7 @@ defineProps<{
     >
       <div class="p-3 font-mono text-sm text-theme-600 md:text-base">
         <div v-for="(output, index) in state.outputs" :key="index">
-          <div v-if="output" class="whitespace-pre-line">{{ output }}</div>
+          <div v-if="output" class="whitespace-pre-line pb-4">{{ output }}</div>
         </div>
       </div>
     </div>
